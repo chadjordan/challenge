9571fb7572bb9a1f576bbc9b3538ce1d2a369513
Ian Pansensoy <ipansensoy@gmail.com>
2015-03-11 08:07:32 +0800

    completed serving in php

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 'use strict';
-
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
@@ -18,191 +17,210 @@ var uncss = require('gulp-uncss');
 var header = require('gulp-header');
 var pkg = require('./package.json');
 var browserSync = require('browser-sync');
+var httpProxy = require('http-proxy');
+var connect = require('gulp-connect-php');
 var reload = browserSync.reload;
 var banner = [
-	'/*!\n' +
-	' * <%= pkg.title %>\n' +
-	' * <%= pkg.description %>\n' +
-	' * <%= pkg.url %>\n' +
-	' * @author <%= pkg.author %>\n' +
-	' * @version <%= pkg.version %>\n' +
-	' * Copyright ' + new Date().getFullYear() + '. <%= pkg.license %> licensed.\n' +
-	' */',
-	'\n'
+    '/*!\n' +
+    ' * <%= pkg.title %>\n' +
+    ' * <%= pkg.description %>\n' +
+    ' * <%= pkg.url %>\n' +
+    ' * @author <%= pkg.author %>\n' +
+    ' * @version <%= pkg.version %>\n' +
+    ' * Copyright ' + new Date().getFullYear() + '. <%= pkg.license %> licensed.\n' +
+    ' */',
+    '\n'
 ].join('');
 
 gulp.task('clean', function() {
-	return gulp.src('build', {
-			read: false
-		})
-		.pipe(clean())
-		.on('error', notify.onError(function(error) {
-			return "Gulp Error: " + error.message;
-		}))
+    return gulp.src('build', {
+            read: false
+        })
+        .pipe(clean())
+        .on('error', notify.onError(function(error) {
+            return "Gulp Error: " + error.message;
+        }))
 });
 
 gulp.task('lint', function() {
-	gulp.src('app/assets/js/**/*.js')
-		.pipe(jshint('.jshintrc'))
-		.pipe(jshint.reporter('jshint-stylish'))
-		.on('error', notify.onError(function(error) {
-			return "Gulp Error: " + error.message;
-		}))
+    gulp.src('app/assets/js/**/*.js')
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('jshint-stylish'))
+        .on('error', notify.onError(function(error) {
+            return "Gulp Error: " + error.message;
+        }))
 });
 
 
 gulp.task('images', function() {
-	return gulp.src(['app/assets/img/**/*.*', '!app/assets/img/icons/**/*.*'])
-		.pipe(imagemin({
-			optimizationLevel: 3,
-			progressive: true,
-			interlaced: true
-		}))
-		.on('error', notify.onError(function(error) {
-			return "Gulp Error: " + error.message;
-		}))
+    return gulp.src(['app/assets/img/**/*.*', '!app/assets/img/icons/**/*.*'])
+        .pipe(imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true
+        }))
+        .on('error', notify.onError(function(error) {
+            return "Gulp Error: " + error.message;
+        }))
 
-	.pipe(gulp.dest('build/assets/img'))
+    .pipe(gulp.dest('build/assets/img'))
 
 
 });
 
 
 gulp.task('compass', function() {
-	return gulp.src('app/assets/sass/*.scss')
-		.pipe(compass({
-			css: 'app/assets/css',
-			sass: 'app/assets/sass',
-			image: 'app/assets/img'
-		}))
-		.on('error', notify.onError(function(error) {
-			return "Gulp Error: " + error.message;
-		}))
+    return gulp.src('app/assets/sass/*.scss')
+        .pipe(compass({
+            css: 'app/assets/css',
+            sass: 'app/assets/sass',
+            image: 'app/assets/img'
+        }))
+        .on('error', notify.onError(function(error) {
+            return "Gulp Error: " + error.message;
+        }))
 
-	.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-		.pipe(minifycss())
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(minifycss())
 
-	.pipe(header(banner, {
-			pkg: pkg
-		}))
-		.pipe(gulp.dest('app/assets/css'));
+    .pipe(header(banner, {
+            pkg: pkg
+        }))
+        .pipe(gulp.dest('app/assets/css'));
 
 });
 
 gulp.task('html', function() {
-	return gulp.src('app/*.html')
-		.pipe(processhtml())
-		.pipe(gulp.dest('build'));
+    return gulp.src(['app/*.html','app/*.php'])
+        .pipe(processhtml())
+        .pipe(gulp.dest('build'));
 });
 
 
 
 gulp.task('scripts', function() {
-	return gulp.src(['app/assets/js/*.js', 'app/assets/js/**/*.js'])
-		.pipe(concat('main.js'))
-		.pipe(rename({
-			suffix: '.min'
-		}))
-		.on('error', notify.onError(function(error) {
-			return "Gulp Error: " + error.message;
-		}))
-		.pipe(uglify())
+    return gulp.src(['app/assets/js/*.js', 'app/assets/js/**/*.js'])
+        .pipe(concat('main.js'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .on('error', notify.onError(function(error) {
+            return "Gulp Error: " + error.message;
+        }))
+        .pipe(uglify())
 
-	.pipe(header(banner, {
-			pkg: pkg
-		}))
-		.pipe(gulp.dest('build/assets/js'));
+    .pipe(header(banner, {
+            pkg: pkg
+        }))
+        .pipe(gulp.dest('build/assets/js'));
 });
 
 
 
 gulp.task('compass-deploy', function() {
-	return gulp.src('app/assets/sass/*.scss')
-		.pipe(compass({
-			css: 'app/assets/css',
-			sass: 'app/assets/sass',
-			image: 'app/assets/img'
-		}))
-		.on('error', notify.onError(function(error) {
-			return "Gulp Error: " + error.message;
-		}))
+    return gulp.src('app/assets/sass/*.scss')
+        .pipe(compass({
+            css: 'app/assets/css',
+            sass: 'app/assets/sass',
+            image: 'app/assets/img'
+        }))
+        .on('error', notify.onError(function(error) {
+            return "Gulp Error: " + error.message;
+        }))
 
-	.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-		.pipe(minifycss())
-		.pipe(rename({
-			suffix: '.min'
-		}))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(minifycss())
+        .pipe(rename({
+            suffix: '.min'
+        }))
 
-	.pipe(header(banner, {
-			pkg: pkg
-		}))
-		.pipe(gulp.dest('build/assets/css'));
+    .pipe(header(banner, {
+            pkg: pkg
+        }))
+        .pipe(gulp.dest('build/assets/css'));
 
 });
 
 
 gulp.task('copy', function() {
-	return gulp.src([
-			'app/**/',
-			'!app/*.html',
-			'!app/assets/css/**',
-			'!app/assets/js/**',
-			'!app/{assets/sass,assets/sass/**}',
-			'!app/assets/img/**'
-		], {
-			dot: true
-		})
-		.on('error', notify.onError(function(error) {
-			return "Gulp Error: " + error.message;
-		}))
-	.pipe(gulp.dest('build'))
+    return gulp.src([
+            'app/**/',
+            '!app/*.html',
+            '!app/*.php',
+            '!app/assets/css/**',
+            '!app/assets/js/**',
+            '!app/{assets/sass,assets/sass/**}',
+            '!app/assets/img/**'
+        ], {
+            dot: true
+        })
+        .on('error', notify.onError(function(error) {
+            return "Gulp Error: " + error.message;
+        }))
+        .pipe(gulp.dest('build'))
 
 });
 
 gulp.task('uncss', function() {
-	return gulp.src('app/assets/css/*.css')
-		.pipe(uncss({
-			html: ['app/*.html'] //add all .html files in 'array', here
-		}))
-		.on('error', notify.onError(function(error) {
-			return "Gulp Error: " + error.message;
-		}))
-		.pipe(minifycss())
-		.pipe(rename({
-			suffix: '.min'
-		}))
+    return gulp.src('app/assets/css/*.css')
+        .pipe(uncss({
+            html: ['app/*.html','app/*.php']
+        }))
+        .on('error', notify.onError(function(error) {
+            return "Gulp Error: " + error.message;
+        }))
+        .pipe(minifycss())
+        .pipe(rename({
+            suffix: '.min'
+        }))
 
-	.pipe(header(banner, {
-			pkg: pkg
-		}))
-		.pipe(gulp.dest('build/assets/css'));
+    .pipe(header(banner, {
+            pkg: pkg
+        }))
+        .pipe(gulp.dest('build/assets/css'));
 });
 
 
 gulp.task('serve', ['compass'], function() {
-	browserSync({
-		notify: false,
-		port: 9002,
-		browser: ["chrome", "firefox"],
-		ui: {
-			port: 9003
-		},
+    connect.server({
+        port: 9001,
+        base: 'app',
+        open: false
+    });
 
-		// Run as an https by uncommenting 'https: true'
-		// Note: this uses an unsigned certificate which on first access
-		//       will present a certificate warning in the browser.
-		// https: true,
-		server: ['app']
-	});
+    var proxy = httpProxy.createProxyServer({});
 
-	gulp.watch(['app/**/*.html'], reload);
-	gulp.watch(['app/assets/sass/**/*.scss'], ['compass', reload]);
-	gulp.watch(['app/assets/js/**/*.js'], ['lint', reload]);
-	gulp.watch(['app/assets/img/**/*.*'], ['compass', reload]);
+    browserSync({
+            notify: false,
+            port: 9000,
+            server: {
+                baseDir: ['app'],
+
+                middleware: function(req, res, next) {
+                    var url = req.url;
+
+                    if (!url.match(/^\/(css|fonts)\//)) {
+                        proxy.web(req, res, {
+                            target: 'http://127.0.0.1:9001'
+                        });
+                    } else {
+                        next();
+                    }
+                }
+            }
+        })
+
+
+    // watch for changes
+    gulp.watch(['app/**/*.html'], reload);
+    gulp.watch(['app/**/*.php'], reload);
+    gulp.watch(['app/assets/sass/**/*.scss'], ['compass', reload]);
+    gulp.watch(['app/assets/js/**/*.js'], ['lint', reload]);
+    gulp.watch(['app/assets/img/**/*.*'], ['compass', reload]);
 });
 
 
 gulp.task('default', ['clean', 'compass', 'lint', 'serve']);
 gulp.task('build', function(cb) {
-	gulpsequence(['clean', 'images', 'scripts', 'compass-deploy', 'html', 'uncss', 'copy'])(cb);
+    gulpsequence(['clean', 'images', 'scripts', 'compass-deploy', 'html', 'uncss', 'copy'])(cb);
 });
