b08c1e1c4daab98dc09063f797afd7703227af24
Ian Pansensoy <ian.pansensoy@nuworks.ph>
2015-03-19 11:35:13 +0800

    fixed image issues

---
+++ build/index.php
@@ -0,0 +1,95 @@
+
+<?php include 'header.php' ;?>
+    <div class="container">
+        <nav class=" navbar navbar-default navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Project name</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav pull-right">
+                        <li class="active"><a href="#">Home</a>
+                        </li>
+                        <li><a href="#about">About</a>
+                        </li>
+                        <li><a href="#contact">Contact</a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="#">Action</a>
+                                </li>
+                                <li><a href="#">Another action</a>
+                                </li>
+                                <li><a href="#">Something else here</a>
+                                </li>
+                                <li class="divider"></li>
+                                <li class="dropdown-header">Nav header</li>
+                                <li><a href="#">Separated link</a>
+                                </li>
+                                <li><a href="#">One more separated link</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+
+                </div>
+                <!--/.nav-collapse -->
+            </div>
+        </nav>
+
+        <div class="jumbotron">
+
+            <h1>Allo Allo !</h1>
+            <p class="lead">Always a pleasure scaffolding your apps.</p>
+            <p><a class="btn btn-lg btn-success" href="#">Splendid! <span class="glyphicon glyphicon-ok"></span></a>
+            </p>
+        </div>
+
+        <div class="row marketing">
+
+            <div class="col-lg-3 col-xs-12">
+                <div class="gulp-bg">
+                    <img class="img-responsive" style="height:180px" height="180" width="180" src="http://gulpjs.com/img/gulp-white-text.svg" alt="gulp js">
+                </div>
+                <h4>Gulp JS</h4>
+
+                <p>Gulp is a fast and intuitive streaming build tool built on Node.js.</p>
+            </div>
+            <div class="col-lg-3 col-xs-12">
+                <div>
+                    <img class="img-responsive" height="180" width="180" src="http://html5boilerplate.com/apple-touch-icon.png" alt="HTML5 Boilerplate">
+                </div>
+                <h4>HTML5 Boilerplate</h4>
+                <p>HTML5 Boilerplate is a professional front-end template for building fast, robust, and adaptable web apps or sites.</p>
+            </div>
+            <div class="col-lg-3 col-xs-12">
+                <div>
+                    <img height="180" width="180" src="http://baltimorewebdesigngroup.org/assets/img/logo.png" alt="Sass-Lang">
+                </div>
+                <h4>Sass</h4>
+                <p>Sass is a mature, stable, and powerful professional grade CSS extension language.</p>
+
+            </div>
+
+
+            <div class="col-lg-3 col-xs-12">
+                <div>
+                    <img class="img-responsive" height="180" width="180" src="http://www.teamstudio.com/Portals/218295/images/bootstrap-logo.png" alt="Bootstrap">
+
+                </div>
+                <h4>Bootstrap</h4>
+                <p>Sleek, intuitive, and powerful mobile first front-end framework for faster and easier web development.</p>
+            </div>
+        </div>
+
+
+
+    </div>
+    <?php include 'footer.php' ;?>
