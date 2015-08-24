4d55c0ba70926f512cf00881bb44d250ed473f61
Chad Jordan Hilis <chadjordan.hilis@gmail.com>
2015-08-24 17:24:27 +0800

    last push

--- build/header.php
+++ build/header.php
@@ -4,11 +4,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>APD Discovery</title>
+    <title>Gulp Front-End Starter Kit</title>
 
-    <!-- build:css assets/css/main.min.css -->
-        <link href="assets/css/main.css" rel="stylesheet">
-    <!-- /build -->
+    <link rel="stylesheet" href="assets/css/main.min.css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
---
+++ build/index.php
@@ -0,0 +1,16 @@
+
+<?php include 'header.php' ;?>
+
+    <div class="splsh">
+        <fieldset>
+            <legend><span>APD</span>DISCOVERY</legend> 
+
+            <div class="bnrCnt">
+                <h1> Blog Insights </h1>
+                <h3>Inspiration, insights & intellegence </h3>
+            </div>
+
+        </fieldset>
+    </div>
+    
+<?php include 'footer.php' ;?>
